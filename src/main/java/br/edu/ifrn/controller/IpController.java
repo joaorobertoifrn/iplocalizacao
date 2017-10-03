0bc74fcfd387fb334a055a3725f5f5c6848b0e63
João Roberto de França Pereira <joao.roberto@academico.ifrn.edu.br>
2017-10-03 15:51:17 -0300

    Correções no projeto, mensagem de erro

--- src/main/java/br/edu/ifrn/controller/IpController.java
+++ src/main/java/br/edu/ifrn/controller/IpController.java
@@ -3,9 +3,11 @@ package br.edu.ifrn.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -26,10 +28,16 @@ public class IpController {
 	private ConsultaIpService consultaIpService;
 	
 	@RequestMapping
-	public ModelAndView pesquisar(@ModelAttribute("filtro") IpFilter filtro) throws JsonParseException, JsonMappingException, UnirestException {
+	public ModelAndView pesquisar(@ModelAttribute("filtro") IpFilter filtro, Errors errors, RedirectAttributes attributes) throws JsonParseException, JsonMappingException, UnirestException {
 		Localizacao localizacao = consultaIpService.filtrar(filtro);
 		ModelAndView mv = new ModelAndView(IP_VIEW);
+		
+		if (!localizacao.getApierrormsg().isEmpty()) {
+			attributes.addFlashAttribute("mensagem", localizacao.getApierrormsg());
+		}
+		
 		mv.addObject(localizacao);
+		
 		return mv;
 	}
 	
